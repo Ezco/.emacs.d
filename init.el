39569bd5bcd2e6e6a660768d795f90b6966d57f2
Ezco <michael.larsson.esko@outlook.com>
2020-01-15 00:48:27 +0100

    update to init.el, and deleted gruvbox package

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
 
 
 ;; UI settings
+(setq inhibit-startup-screen t)
 (setq visible-bell 1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -62,12 +63,13 @@
   (c-toggle-auto-hungry-state 1))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (json-mode gruvbox-theme evil))))
+ '(package-selected-packages (quote (json-mode evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
