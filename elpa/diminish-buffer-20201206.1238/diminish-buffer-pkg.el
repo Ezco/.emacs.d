2502a2c3136dd122ad7bbba1abc5c2f7ba54148a
Ezco <michael.larsson.esko@outlook.com>
2021-01-17 01:15:06 +0100

    rm unnecessary files

--- elpa/diminish-buffer-20201206.1238/diminish-buffer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "diminish-buffer" "20201206.1238" "Diminish (hide) buffers from buffer-menu" '((emacs "24.4")) :commit "387858aaa23d5d1145c98dfa70bbd39bb8c3fa5f" :authors '(("Shen, Jen-Chieh" . "jcs090218@gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :url "https://github.com/jcs-elpa/diminish-buffer")
